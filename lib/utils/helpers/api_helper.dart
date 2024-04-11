78b168cead305052db45b35bffc5b932d108f57d
sumitFlutter <gohilsumit142@gmail.com>
2024-04-11 11:25:18 +0530

    update 2

--- lib/utils/helpers/api_helper.dart
+++ lib/utils/helpers/api_helper.dart
@@ -1,9 +1,5 @@
 import 'dart:convert';
-import 'dart:ui';
-
-import 'package:flutter/services.dart';
 import 'package:http/http.dart'as http;
-import 'package:translate_api_app/screen/model/language_model.dart';
 class APIHelper{
   Future<String> translateData({required String text,required String source,required String target})
   async {
@@ -12,11 +8,16 @@ class APIHelper{
       "source": source,
       "target": target
     };
+    Map<String,String>? header= {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '090af0fcf9msh052e8d02c1d238dp1f0666jsn5ff1fc6b8758',
+      'X-RapidAPI-Host': 'translate-plus.p.rapidapi.com'
+    };
     var jsonData=jsonEncode(bodyJson);
-    var result=await http.post(Uri.parse("https://translate-plus.p.rapidapi.com/translate"),body: jsonData,headers: {"X-RapidAPI-Key":"090af0fcf9msh052e8d02c1d238dp1f0666jsn5ff1fc6b8758","X-RapidAPI-Host": "translate-plus.p.rapidapi.com","content-type": "application/json"});
+    var result=await http.post(Uri.parse("https://translate-plus.p.rapidapi.com/translate"),body: jsonData,headers:header );
     if(result.statusCode==200)
       {
-        var mainJson=jsonDecode(utf8.decode(result.bodyBytes));
+        var mainJson=await jsonDecode(utf8.decode(result.bodyBytes));
         String ans=mainJson["translations"]["translation"];
         return ans;
       }
@@ -30,10 +31,47 @@ class APIHelper{
     });
     if(result.statusCode==200)
       {
-        var mainJson=jsonDecode(result.body);
+        var mainJson=await jsonDecode(result.body);
         Map lMap=mainJson["supported_languages"];
         return lMap;
       }
     return {};
   }
 }
+/* axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://translate-plus.p.rapidapi.com/',
+  headers:
+};
+
+try {
+const response = await axios.request(options);
+console.log(response.data);
+} catch (error) {
+console.error(error);
+const axios = require('axios');
+
+const options = {
+  method: 'POST',
+  url: 'https://translate-plus.p.rapidapi.com/translate',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': '090af0fcf9msh052e8d02c1d238dp1f0666jsn5ff1fc6b8758',
+    'X-RapidAPI-Host': 'translate-plus.p.rapidapi.com'
+  },
+  data: {
+    text: 'Hello , How are you',
+    source: 'en',
+    target: 'nl'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+}*/
